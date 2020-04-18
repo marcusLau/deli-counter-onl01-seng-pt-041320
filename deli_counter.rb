4fdacc6c6ece9f6ce34f82c5a9da1e68f5cabc3b
Marcus Lau <lau.marcus76@gmail.com>
2020-04-18 19:42:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,6 @@ def line(queue)
   if queue.length == 0
     puts "The line is currently empty."
   else
-    
     # make an array with [1. Ada, 2. Ken, 3. Lol] 
     for i in 0..queue.size-1
       pos << "#{i+1}. #{queue[i]}"
