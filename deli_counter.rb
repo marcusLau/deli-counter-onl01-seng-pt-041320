a75a99bd9342b48f30e3c0ba420f8b5dee04ee24
Marcus Lau <lau.marcus76@gmail.com>
2020-04-18 19:24:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,11 @@
 # Write your code here.
 
 def line(queue)
-  if queue.length 
+  if queue.length > 0 
+    # iterate thru w/ for and print each person's place
+    for i in 0..queue.size-1
+      "The line is currently:"
+  else
+    "The line is currently empty"
+  end
+end
