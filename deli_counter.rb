832e05ccd1e2b80925217a6e3042a49079b26236
Marcus Lau <lau.marcus76@gmail.com>
2020-04-18 19:42:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,14 @@
 # Write your code here.
 
 def line(queue)
-  if queue.length > 0 
+  if queue.length == 0
+    puts "The line is currently empty."
+  else
     pos = []
     # make an array with [1. Ada, 2. Ken, 3. Lol] 
     for i in 0..queue.size-1
       pos << "#{i+1}. #{queue[i]}"
     end
-  else
-    puts "The line is currently empty."
   end
   puts "The line is currently: #{pos.join(" ")}"
 end
